9bab7b6cee9be4e30e98c2386e38fd3fecef1bca
PeregudovSergey <peregudov.sv@phystech.edu>
2022-04-03 23:50:42 +0300

    delete build

--- task3/bld.sh
+++ task3/bld.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+mkdir build
 cp start.sh build/start.sh
 rm -r build/*
 cp start.sh build/start.sh
--- task3/build/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cmake -DTOOLCHAIN=~/Desktop/toolchain/bin -DCMAKE_INSTALL_PREFIX=~/task3/TP/task3/install ..
-make
-make install
